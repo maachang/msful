30188d973bdde2fc206d281b6588d1b09c992761
masahito suzuki <masahito.suzuki@supership.jp>
2019-06-02 22:53:37 +0900

    base_loggerを修正

--- msful/logger.js
+++ msful/logger.js
@@ -22,43 +22,43 @@ module.exports = (function () {
       isTraceEnabled: function() {
         return _lg.isTraceEnabled();
       },
-      trace: function(m, e) {
-        _lg.trace(m, e);
+      trace: function() {
+        _lg.trace.apply(null, arguments)
         return this;
       },
       isDebugEnabled: function() {
         return _lg.isDebugEnabled();
       },
-      debug: function(m, e) {
-        _lg.debug(m, e);
+      debug: function() {
+        _lg.debug.apply(null, arguments)
         return this;
       },
       isInfoEnabled: function() {
         return _lg.isInfoEnabled();
       },
-      info: function(m, e) {
-        _lg.info(m, e);
+      info: function() {
+        _lg.info.apply(null, arguments)
         return this;
       },
       isWarnEnabled: function() {
         return _lg.isWarnEnabled();
       },
-      warn: function(m, e) {
-        _lg.warn(m, e);
+      warn: function() {
+        _lg.warn.apply(null, arguments)
         return this;
       },
       isErrorEnabled: function() {
         return _lg.isErrorEnabled();
       },
-      error: function(m, e) {
-        _lg.error(m, e);
+      error: function() {
+        _lg.error.apply(null, arguments)
         return this;
       },
       isFatalEnabled: function() {
         return _lg.isFatalEnabled();
       },
-      fatal: function(m, e) {
-        _lg.fatal(m, e);
+      fatal: function() {
+        _lg.fatal.apply(null, arguments)
         return this;
       }
     }
@@ -69,17 +69,17 @@ module.exports = (function () {
   // 空のロガー.
   var _BLANK = Object.freeze({
     isTraceEnabled: function() { return true; },
-    trace: function(m, e) {return this;},
+    trace: function() {return this;},
     isDebugEnabled: function() { return true; },
-    debug: function(m, e) {return this;},
+    debug: function() {return this;},
     isInfoEnabled: function() { return true; },
-    info: function(m, e) {return this;},
+    info: function() {return this;},
     isWarnEnabled: function() { return true; },
-    warn: function(m, e) {return this;},
+    warn: function() {return this;},
     isErrorEnabled: function() { return true; },
-    error: function(m, e) {return this;},
+    error: function() {return this;},
     isFatalEnabled: function() { return true; },
-    fatal: function(m, e) {return this;}
+    fatal: function() {return this;}
   });
 
   // ロガーを取得.
